f777bcf85e7aa14fa42ce57c31831d7ccda48eae
Ezz <Ezztherose@gmail.com>
2021-02-19 15:14:06 +0100

    setup packages and testing everything to work

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import speech_recognition as sr
+import pyttsx3
+
+lisening = sr.Recognizer()
+
+try:
+    with sr.Microphone() as src:
+        print('lisening...')
+        voice = lisening.listen(src)
+        com = lisening.recognize_google(voice)
+        print(com)
+except:
+    print('exception')
